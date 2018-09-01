8f87ec6d8d21b047fa4e6449525869c9c180da3b
wektor1 <wektor1@gmail.com>
2018-09-01 23:09:38 +0200

    5# Counting points in completed frames

---
+++ inc/ScoreInCompleted.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+class ScoreInCompleted
+{
+    int score_, ball_score_, ball_score_multiplier_,
+        second_ball_score_multiplier_, third_ball_score_multiplier_;
+    std::string game_;
+    void isStrike(const char ball);
+    void isMissed(const char ball);
+    void isSpare(const char ball);
+    void isNormalHit(const char ball);
+    void checkCharacter(const char character, int &value);
+    int calculatePoints(std::string::const_iterator begin,
+                        std::string::const_iterator end);
+    void endScore();
+
+public:
+    ScoreInCompleted(std::string game);
+    ~ScoreInCompleted();
+    int getScore();
+};
---
+++ src/ScoreInCompleted.cpp
@@ -0,0 +1,75 @@
+#include "../inc/ScoreInCompleted.hpp"
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+
+ScoreInCompleted::ScoreInCompleted(std::string game) : game_(game)
+{
+    endScore();
+}
+
+ScoreInCompleted::~ScoreInCompleted() {}
+
+int ScoreInCompleted::getScore() { return score_; }
+
+void ScoreInCompleted::isStrike(const char ball)
+{
+    if (ball == 'x') {
+        ball_score_ = 10;
+        second_ball_score_multiplier_++;
+        third_ball_score_multiplier_++;
+    }
+}
+
+void ScoreInCompleted::isSpare(const char ball)
+{
+    if (ball == '/') {
+        ball_score_ = 10 - ball_score_;
+        second_ball_score_multiplier_++;
+    }
+}
+
+void ScoreInCompleted::isMissed(const char ball)
+{
+    if (ball == '-')
+        ball_score_ = 0;
+}
+
+void ScoreInCompleted::isNormalHit(const char ball)
+{
+    if (isdigit(ball))
+        ball_score_ = std::atoi(&ball);
+}
+
+void ScoreInCompleted::checkCharacter(const char character, int &value)
+{
+    if (character != '|') {
+        isSpare(character);
+        isStrike(character);
+        isMissed(character);
+        isNormalHit(character);
+        value += ball_score_ * ball_score_multiplier_;
+        ball_score_multiplier_ = second_ball_score_multiplier_;
+        second_ball_score_multiplier_ = third_ball_score_multiplier_;
+        third_ball_score_multiplier_ = 1;
+    }
+}
+
+int ScoreInCompleted::calculatePoints(std::string::const_iterator begin,
+                                      std::string::const_iterator end)
+{
+    int value = 0;
+    ball_score_multiplier_ = third_ball_score_multiplier_
+        = second_ball_score_multiplier_ = 1;
+    ball_score_ = 0;
+    std::for_each(begin, end,
+                  [&](const char ball) { checkCharacter(ball, value); });
+    return value;
+}
+
+void ScoreInCompleted::endScore()
+{
+    score_ = calculatePoints(game_.begin(), game_.end());
+    auto normal_frames_end = (game_.end() - 2);
+    score_ -= calculatePoints(normal_frames_end, game_.end());
+}
