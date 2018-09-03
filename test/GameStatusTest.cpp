6ffa64c57c3d6d46f0e34ea680325de953ce25ee
LuckyCode7 <nowak9.private@gmail.com>
2018-09-03 12:16:32 +0200

    7# game status

--- test/GameStatusTest.cpp
+++ test/GameStatusTest.cpp
@@ -7,11 +7,51 @@ struct GameStatusTest : public ::testing::Test
 TEST_F(GameStatusTest, 1_check_method_getStatus)
 {
     // GIVEN
+    std::string path("../results/Game1.txt");
+    // WHEN
+    Status status(path);
+    // THEN
+    ASSERT_EQ(status.getStatus(), "game in progress");
+}
+
+TEST_F(GameStatusTest, 2_check_method_getStatus)
+{
+    // GIVEN
+    std::string path("../results/Game2.txt");
+    // WHEN
+    Status status(path);
+    // THEN
+    ASSERT_EQ(status.getStatus(), "game in progress");
+}
+
+TEST_F(GameStatusTest, 3_check_method_getStatus)
+{
+    // GIVEN
+    std::string path("../results/Game3.txt");
+    // WHEN
+    Status status(path);
+    // THEN
+    ASSERT_EQ(status.getStatus(), "no game");
+}
+
+TEST_F(GameStatusTest, 4_check_method_getStatus)
+{
+    // GIVEN
     std::string path("../results/Game4.txt");
     // WHEN
     Status status(path);
     // THEN
     ASSERT_EQ(status.getStatus(), "game finished");
+}
+
+TEST_F(GameStatusTest, 5_check_method_getStatus)
+{
+    // GIVEN
+    std::string path("../results/Game4.txt");
+    // WHEN
+    Status status(path);
+    // THEN
+    ASSERT_EQ(status.getStatus(), "no game");
 }
 
 
