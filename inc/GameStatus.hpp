0dfa7705138b5d98d1f548cabfd0ef82322a7a0d
LuckyCode7 <nowak9.private@gmail.com>
2018-09-13 14:28:11 +0200

    const static class member added

--- inc/GameStatus.hpp
+++ inc/GameStatus.hpp
@@ -7,14 +7,19 @@ class Status
 private:
     std::vector<std::string> allPlayersResults;
     std::vector<std::string> allPlayersStatus;
+    std::string statusAnalyzer() const;
     void fileAnalyzer(const std::string& result);
     void checkStrike(const std::string& result);
     void checkSpare(const std::string& result);
     void checkExtraBall(const std::string& result);
+    void changeToLower();
     bool checkIfGameIsInProggress(const std::string& result);
     bool checkEmptyFile(const std::string& result);
-    std::string statusAnalyzer() const;
-    void changeToLower();
+
+    static const std::string gameInProgress;
+    static const std::string noGame;
+    static const std::string gameFinished;
+
 public:
     explicit Status(const std::vector<std::string>& allPlayersResults_);
     std::string getStatus();
--- src/GameStatus.cpp
+++ src/GameStatus.cpp
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <locale>
 
+const std::string Status::gameInProgress = "game in progress";
+const std::string Status::noGame = "no game";
+const std::string Status::gameFinished = "game finished";
+
 Status::Status(const std::vector<std::string>& allPlayersResults_) :
     allPlayersResults(allPlayersResults_)
 {
@@ -15,65 +19,65 @@ void Status::changeToLower()
 {
     std::for_each(allPlayersResults.begin(),allPlayersResults.end(),[](std::string& line)
     {
-       std::for_each(line.begin(),line.end(),[](char& sign)
-       {
-           sign=std::tolower(sign);
-       });
+        std::for_each(line.begin(),line.end(),[](char& sign)
+        {
+            sign = std::tolower(sign);
+        });
     });
 }
 
 void Status::checkStrike(const std::string& result)
 {
     if (result.size() == result.find_last_of('|') + 1)
-        allPlayersStatus.push_back("game in progress");
+        allPlayersStatus.push_back(gameInProgress);
     else if (result[result.find_last_of('|') + 1] == 'x')
-        allPlayersStatus.push_back("game finished");
+        allPlayersStatus.push_back(gameFinished);
     else
-        allPlayersStatus.push_back("game in progress");
+        allPlayersStatus.push_back(gameInProgress);
 }
 
 void Status::checkSpare(const std::string& result)
 {
     if (result.size() == result.find_last_of('|') + 1)
-        allPlayersStatus.push_back("game in progress");
+        allPlayersStatus.push_back(gameInProgress);
     else if (result.size() == result.find_last_of('|') + 2)
-        allPlayersStatus.push_back("game finished");
+        allPlayersStatus.push_back(gameFinished);
 }
 
- bool Status::checkEmptyFile(const std::string& result)
- {
-     if (result == "")
-     {
-         allPlayersStatus.push_back("no game");
-         return true;
-     }
-     return false;
- }
+bool Status::checkEmptyFile(const std::string& result)
+{
+    if (result == "")
+    {
+        allPlayersStatus.push_back(noGame);
+        return true;
+    }
+    return false;
+}
 
- bool Status::checkIfGameIsInProggress(const std::string& result)
- {
-     if (std::count(result.begin(), result.end(), '|') < 11)
-     {
-         allPlayersStatus.push_back("game in progress");
-         return true;
-     }
-     return false;
- }
+bool Status::checkIfGameIsInProggress(const std::string& result)
+{
+    if (std::count(result.begin(), result.end(), '|') < 11)
+    {
+        allPlayersStatus.push_back(gameInProgress);
+        return true;
+    }
+    return false;
+}
 
- void Status::checkExtraBall(const std::string& result)
- {
-     switch (result[result.find_last_of('|') - 2])
-     {
-     case 'x':
-         checkStrike(result);
-         break;
-     case '/':
+void Status::checkExtraBall(const std::string& result)
+{
+    switch (result[result.find_last_of('|') - 2])
+    {
+    case 'x':
+        checkStrike(result);
+        break;
+    case '/':
         checkSpare(result);
-         break;
-     default:
-         allPlayersStatus.push_back("game finished");
-     }
- }
+        break;
+    default:
+        allPlayersStatus.push_back(gameFinished);
+    }
+}
 
 void Status::fileAnalyzer(const std::string& result)
 {
@@ -84,15 +88,15 @@ void Status::fileAnalyzer(const std::string& result)
 
 std::string Status::statusAnalyzer() const
 {
-    if (std::count(allPlayersStatus.begin(), allPlayersStatus.end(), "no game") == 1)
+    if (std::count(allPlayersStatus.begin(), allPlayersStatus.end(), noGame) == 1)
     {
-        return "no game";
+        return noGame;
     }
-    if (std::count(allPlayersStatus.begin(), allPlayersStatus.end(), "game in progress") > 0)
+    if (std::count(allPlayersStatus.begin(), allPlayersStatus.end(), gameInProgress) > 0)
     {
-        return "game in progress";
+        return gameInProgress;
     }
-    return "game finished";
+    return gameFinished;
 }
 
 std::string Status::getStatus()
